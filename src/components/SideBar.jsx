7f9ed86dc9e302c498757650cccaf09a0df8f7ee
ftvalex01 <ftvalex@hotmail.com>
2024-02-11 12:19:06 +0000

    arreglado el responsive del sidebar en todos los tamaños

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -13,6 +13,7 @@ import {
   FaRegHeart,
   FaPlusSquare
 } from 'react-icons/fa';
+
 const SideBar = () => {
   const { logout, user } = useAuthContext();
   const [isModalOpen, setModalOpen] = useState(false);
@@ -25,72 +26,54 @@ const SideBar = () => {
 
   return (
     <>
-      {/* Sidebar para pantallas grandes */}
-      <aside className="hidden sm:flex flex-col bg-gray-700 fixed inset-y-0 left-0 z-30 transition-width duration-300 w-16 md:w-40 lg:w-64 xl:w-72">
+      {/* Sidebar para pantallas medianas y grandes */}
+      <aside className="hidden lg:flex flex-col bg-gray-700 fixed inset-y-0 left-0 z-30 w-64">
         <div className="flex flex-col space-y-6 p-4">
-          <h1 className="text-3xl font-bold mb-4 hidden lg:block text-white">MyPic</h1>
+          <h1 className="text-3xl font-bold mb-4 text-white">MyPic</h1>
           <nav className="flex flex-col space-y-6">
             <Link to="/" className="flex items-center space-x-2 text-white">
               <FaHome className="text-lg" />
-              <span className="hidden md:inline">Inicio</span>
+              <span>Inicio</span>
             </Link>
             <Link to="#" onClick={openSearchModal} className="flex items-center space-x-2 text-white">
               <FaSearch className="text-lg" />
-              <span className="hidden md:inline">Búsqueda</span>
+              <span>Búsqueda</span>
             </Link>
             <Link to="/explore" className="flex items-center space-x-2 text-white">
               <FaRegCompass className="text-lg" />
-              <span className="hidden md:inline">Explorar</span>
+              <span>Explorar</span>
             </Link>
             <Link to="/reels" className="flex items-center space-x-2 text-white">
               <RiLiveLine className="text-lg" />
-              <span className="hidden md:inline">Reels</span>
+              <span>Reels</span>
             </Link>
             <Link to="/messages" className="flex items-center space-x-2 text-white">
               <MdMessage className="text-lg" />
-              <span className="hidden md:inline">Mensajes</span>
+              <span>Mensajes</span>
             </Link>
             <Link to="/notifications" className="flex items-center space-x-2 text-white">
               <FaRegHeart className="text-lg" />
-              <span className="hidden md:inline">Notificaciones</span>
+              <span>Notificaciones</span>
             </Link>
             <Link to="#" onClick={openModal} className="flex items-center space-x-2 text-white">
               <FaPlusSquare className="text-lg" />
-              <span className="hidden md:inline">Crear</span>
+              <span>Crear</span>
             </Link>
             <Link to="/profile" className="flex items-center space-x-2 text-white">
               <CgProfile className="text-lg" />
-              <span className="hidden md:inline">{user?.data.username}</span>
+              <span>{user?.data.username}</span>
+            </Link>
+            <Link onClick={logout} to="/" className="flex items-center space-x-2 text-white">
+              <MdOutlineMoreHoriz className="text-lg" />
+              <span>Logout</span>
+            </Link>
+            <Link to="/more" className="flex items-center space-x-2 text-white">
+              <MdOutlineMoreHoriz className="text-lg" />
+              <span>Más</span>
             </Link>
           </nav>
         </div>
-        <div className="flex flex-col space-y-6 p-4 mb-4">
-          <Link onClick={logout} to="/" className="flex items-center space-x-2 text-white">
-            <MdOutlineMoreHoriz className="text-lg" />
-            <span className="hidden md:inline">Logout</span>
-          </Link>
-          <Link to="/more" className="flex items-center space-x-2 text-white">
-            <MdOutlineMoreHoriz className="text-lg" />
-            <span className="hidden md:inline">Más</span>
-          </Link>
-        </div>
       </aside>
-
-      {/* Barra de navegación para pantallas pequeñas */}
-      <nav className="sm:hidden flex justify-around text-2xl bg-white w-full fixed inset-x-0 bottom-0 border-t-2 border-gray-200 py-2">
-        <Link to="/" className="text-gray-700">
-          <FaHome />
-        </Link>
-        <Link to="/create" className="text-gray-700">
-          <FaPlusSquare />
-        </Link>
-        <Link to="/notifications" className="text-gray-700">
-          <FaRegHeart />
-        </Link>
-        <Link to="/profile" className="text-gray-700">
-          <CgProfile />
-        </Link>
-      </nav>
 
       {/* Modal para subir */}
       <UploadModal isOpen={isModalOpen} onClose={closeModal} />
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,41 +1,18 @@
-
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/SideBar';
-import BuscarPerfil from '../components/BuscarPerfil';
-import { FaRegHeart } from 'react-icons/fa';
+import MobileNav from '../components/MobileNav'; // Componente de barra de navegación inferior para dispositivos móviles
+import TopBar from '../components/TopBar'; // Componente de barra superior para dispositivos móviles
+
 
 const Layout = () => {
   return (
-    <div className="flex flex-col bg-gray-100 min-h-screen overflow-hidden">
-      {/* Barra superior para dispositivos móviles */}
-      <div className="md:hidden flex justify-between items-center p-2 bg-black text-white fixed top-0 inset-x-0 z-10">
-        <div className="flex items-center space-x-4">
-          <span className="text-3xl font-bold">MyPic</span>
-          <BuscarPerfil />
-        </div>
-        <FaRegHeart className="text-lg" />
-      </div>
-
-      <div className="flex-1 flex flex-col  overflow-auto">
-        {/* Sidebar: Menú para pantallas medianas y grandes, oculto en móviles */}
-        <div className="hidden md:flex md:flex-col md:w-32 bg-black text-white fixed inset-y-0 z-20">
-          <Sidebar />
-        </div>
-
-        {/* Contenedor Principal: Aquí va el contenido principal con scroll */}
-        <div className="flex-1 flex flex-col overflow-auto md:ml-64 pt-4 pb-16 md:pb-0">
-          <div className="pt-4 flex-1">
-            <Outlet />
-          </div>
-        </div>
-      </div>
-
-      {/* Barra Lateral Derecha: Barra Lateral de Usuarios/Sugerencias, visible solo en grandes pantallas */}
-
-      {/* Sidebar en el pie de página para dispositivos móviles */}
-      <div className="md:hidden fixed inset-x-0 bottom-0 z-10">
-        <Sidebar />
-      </div>
+    <div className="bg-gray-100 min-h-screen overflow-hidden">
+      <TopBar />
+      <Sidebar />
+      <main className="pt-16 lg:pl-64 flex flex-col flex-1 overflow-auto">
+        <Outlet />
+      </main>
+      <MobileNav />
     </div>
   );
 };
