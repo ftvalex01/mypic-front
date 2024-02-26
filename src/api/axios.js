7b5a032ff5d2d2ff63ee5e33043ee61ad2fb00a5
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-02-26 04:35:20 +0000

    cambios axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,32 +1,33 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://lucas.informaticamajada.es/",
   withCredentials: true,
- /*  withXSRFToken: true,
-  xsrfCookieName: "XSRF-TOKEN",
-  xsrfHeaderName: "X-XSRF-TOKEN", */
   headers: {
     Accept: "application/json",
-    // Agrega aquí tus encabezados personalizados si es necesario
   }
 });
 
-axios.interceptors.response.use(response => response, async error => {
-  // Verifica si es un error de CSRF, de sesión o un error 401 específicamente en la ruta de verificación de usuario
+axios.interceptors.response.use(response => response, async (error) => {
+  // Verifica si es un error de CSRF o de sesión y si no es una petición a '/api/user'
   if ((error.response && (error.response.status === 419 || error.response.status === 401)) && !error.config.url.endsWith('/api/user')) {
-    // Intenta obtener un nuevo token CSRF solo si no estamos haciendo la petición de verificación de sesión
-    try {
-      await axios.get('/sanctum/csrf-cookie');
-      // Reintenta la petición original
-      return axios.request(error.config);
-    } catch (csrfError) {
-      return Promise.reject(csrfError);
+    // Verifica si ya se ha intentado reenviar la petición
+    const retryCount = error.config.__retryCount || 0;
+    if (retryCount < 2) { // Limita el número de reintentos a 2
+      error.config.__retryCount = retryCount + 1; // Incrementa el contador de reintentos
+      try {
+        // Intenta obtener un nuevo token CSRF
+        await axios.get('/sanctum/csrf-cookie');
+        // Reintenta la petición original con el nuevo token CSRF
+        return axios(error.config);
+      } catch (csrfError) {
+        // Si falla la obtención del token CSRF, rechaza la promesa
+        return Promise.reject(csrfError);
+      }
     }
   }
-  // Para otros tipos de errores o si estamos verificando la sesión, no reintentes la petición automáticamente
+  // Para otros tipos de errores o si se alcanza el límite de reintentos, rechaza la promesa
   return Promise.reject(error);
 });
-
 
 export default axios;
