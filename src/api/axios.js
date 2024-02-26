0559177d50c725d3e1f23cdc2158634e577aadf1
LucasVega2004 <luquitasvegablanco@gmail.com>
2024-02-26 05:06:09 +0000

    axios 6

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,22 +1,32 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: 'https://lucas.informaticamajada.es/',
-  withXSRFToken: true,
+  baseURL: "https://lucas.informaticamajada.es/",
   withCredentials: true,
-  xsrfCookieName: 'XSRF-TOKEN',
-  xsrfHeaderName: 'X-XSRF-TOKEN',
+  withXSRFToken: true,
+  xsrfCookieName: "XSRF-TOKEN",
+  xsrfHeaderName: "X-XSRF-TOKEN",
   headers: {
-    Accept: 'application/json',
+    Accept: "application/json",
+    // Agrega aquí tus encabezados personalizados si es necesario
   }
 });
-// Add a response interceptor
-axios.interceptors.response.use((response) => {
-  // If the request was successful, return the response
-  return response;
-}, (error) => {
-  // If there's an error gin the response, handle it here
-  throw error;
+
+axios.interceptors.response.use(response => response, async error => {
+  // Verifica si es un error de CSRF, de sesión o un error 401 específicamente en la ruta de verificación de usuario
+  if ((error.response && (error.response.status === 419 || error.response.status === 401)) && !error.config.url.endsWith('/api/user')) {
+    // Intenta obtener un nuevo token CSRF solo si no estamos haciendo la petición de verificación de sesión
+    try {
+      await axios.get('/sanctum/csrf-cookie');
+      // Reintenta la petición original
+      return axios.request(error.config);
+    } catch (csrfError) {
+      return Promise.reject(csrfError);
+    }
+  }
+  // Para otros tipos de errores o si estamos verificando la sesión, no reintentes la petición automáticamente
+  return Promise.reject(error);
 });
+
 
 export default axios;
